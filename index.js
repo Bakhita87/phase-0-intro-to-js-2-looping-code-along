82edaf60faa33cff717fcba0710390dd6cbd965f
Bakhita87 <bakhitaotieno@gmail.com>
2023-07-09 11:08:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  let myThankYouCards = [];
+  for (let i = 0; i < names.length; i++) {
+    myThankYouCards.push(
+      `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+    );
+  }
+  return myThankYouCards;
+}
+writeCards(["Jane", "John", "Mary"], "gift");
+
+
+
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
+countDown(10);
+
+
+
+
+
+ 
